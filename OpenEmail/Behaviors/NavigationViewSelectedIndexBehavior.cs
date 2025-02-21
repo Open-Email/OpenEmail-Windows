f27092b4e8aa649b784342066efb6c824455b9dd
Burak Kaan Köse <bkaankose@outlook.com>
2025-02-21 18:25:25 +0100

    Fixed an issue where initial selection on settings was not reflected to UI.

--- OpenEmail/Behaviors/NavigationViewSelectedIndexBehavior.cs
+++ OpenEmail/Behaviors/NavigationViewSelectedIndexBehavior.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using Microsoft.Xaml.Interactivity;
 
 namespace OpenEmail.Behaviors
@@ -11,25 +12,42 @@ namespace OpenEmail.Behaviors
             set { SetValue(SelectedIndexProperty, value); }
         }
 
-        public static readonly DependencyProperty SelectedIndexProperty = DependencyProperty.Register(nameof(SelectedIndex), typeof(int), typeof(NavigationViewSelectedIndexBehavior), new PropertyMetadata(0));
+        public static readonly DependencyProperty SelectedIndexProperty = DependencyProperty.Register(nameof(SelectedIndex), typeof(int), typeof(NavigationViewSelectedIndexBehavior), new PropertyMetadata(-1, new PropertyChangedCallback(OnSelectedIndexChanged)));
 
         public DependencyObject AssociatedObject { get; set; }
 
         public void Attach(DependencyObject associatedObject)
         {
             this.AssociatedObject = associatedObject;
-            var navigationView = (Microsoft.UI.Xaml.Controls.NavigationView)this.AssociatedObject;
+            var navigationView = (NavigationView)this.AssociatedObject;
             navigationView.SelectionChanged += MenuSelectionChanged;
         }
 
-        private void MenuSelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
+        private static void OnSelectedIndexChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var behavior = (NavigationViewSelectedIndexBehavior)d;
+            var navigationView = (NavigationView)behavior.AssociatedObject;
+            navigationView.SelectedItem = navigationView.MenuItems[behavior.SelectedIndex];
+
+            if (navigationView.SelectedItem != null)
+            {
+                var container = navigationView.ContainerFromMenuItem(navigationView.SelectedItem);
+
+                if (container is NavigationViewItem navigationViewItem)
+                {
+                    navigationViewItem.IsSelected = true;
+                }
+            }
+        }
+
+        private void MenuSelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
             this.SelectedIndex = sender.MenuItems.IndexOf(sender.SelectedItem);
         }
 
         public void Detach()
         {
-            var navigationView = (Microsoft.UI.Xaml.Controls.NavigationView)this.AssociatedObject;
+            var navigationView = (NavigationView)this.AssociatedObject;
             navigationView.SelectionChanged -= MenuSelectionChanged;
         }
     }
